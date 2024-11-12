18ddd2fa827629d4800b20dad2de101e1cbf018a
ballll16 <ahmadhamidbalya@gmail.com>
2024-11-13 05:42:46 +0700

    🐎 ci(docker): docker config change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import daStyle from "eslint-config-dicodingacademy";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import daStyle from 'eslint-config-dicodingacademy';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [daStyle, { files: ["**/*.{js,mjs,cjs,ts}"] }, { languageOptions: { globals: globals.node } }, pluginJs.configs.recommended, ...tseslint.configs.recommended];
+export default [
+  daStyle,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
-import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 
 // Create a new express application instance
 const app = express();
-const prisma = new PrismaClient();
 
 // Set the network port
 const port = process.env.PORT || 3000;
 
 // Define the root path with a greeting message
 app.get('/', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
   res.json({
     error: false,
     message: 'Welcome to the Express + TypeScript Server!',
-    users,
   });
 });
 
