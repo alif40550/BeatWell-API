4801254a226b927cdcc69628cfdbad3df6c7d7eb
Alifian <alif40550@gmail.com>
2024-11-12 14:44:46 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import daStyle from "eslint-config-dicodingacademy";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [daStyle, { files: ["**/*.{js,mjs,cjs,ts}"] }, { languageOptions: { globals: globals.node } }, pluginJs.configs.recommended, ...tseslint.configs.recommended];
