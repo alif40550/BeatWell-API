9a67f08b0f242ca6b17b44f8e5ab70e229b23bda
ballll16 <ahmadhamidbalya@gmail.com>
2024-11-12 17:11:32 +0700

    🐞 fix(db): try to select user

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import 'dotenv/config'
-import express, { Request, Response } from "express";
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
 
 // Create a new express application instance
 const app = express();
+const prisma = new PrismaClient();
 
 // Set the network port
 const port = process.env.PORT || 3000;
 
 // Define the root path with a greeting message
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to the Express + TypeScript Server!" });
+app.get('/', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json({
+    error: false,
+    message: 'Welcome to the Express + TypeScript Server!',
+    users,
+  });
 });
 
 // Start the Express server
