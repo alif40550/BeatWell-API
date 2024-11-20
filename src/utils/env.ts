6b49785b1a1f5cdc78ddab16d622df7586c04a05
ballll16 <ahmadhamidbalya@gmail.com>
2024-11-20 22:47:14 +0700

    🦄 refactor(env): group to env.ts

---
+++ src/utils/env.ts
@@ -0,0 +1,4 @@
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const PORT = process.env.PORT;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const PREDICTION_MODEL_URL = process.env.PREDICTION_MODEL_URL;
