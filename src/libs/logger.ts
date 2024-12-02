d48b0cad23cc6f03ba4eabb58d76a1a72d8289e8
ballll16 <ahmadhamidbalya@gmail.com>
2024-12-02 23:28:06 +0700

    ✨ feat(logging): init

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,3 +1,20 @@
-// make GCLog
+import winston from 'winston';
 
-// make windstone
+// import { LoggingWinston } from '@google-cloud/logging-winston';
+
+// const loggingWinston = new LoggingWinston();
+
+export const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `[${timestamp}] ${level}: ${message}`;
+        }),
+        winston.format.colorize({ all: true })
+      ),
+    }),
+    // loggingWinston,
+  ],
+});
