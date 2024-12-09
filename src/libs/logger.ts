e905566a59c451e134e7020aef3121e6d2be1836
Alifian <alif40550@gmail.com>
2024-12-09 17:40:42 +0700

    add logger conf

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,20 +1,19 @@
 import winston from 'winston';
 
-// import { LoggingWinston } from '@google-cloud/logging-winston';
-
-// const loggingWinston = new LoggingWinston();
-
 export const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
+        winston.format.errors({ stack: true }),
+        winston.format.printf(({ timestamp, level, message, errors }) => {
+          if (errors instanceof Error) {
+            return `[${timestamp}] ${level}: ${message} - ${errors.message}`;
+          }
           return `[${timestamp}] ${level}: ${message}`;
         }),
         winston.format.colorize({ all: true })
       ),
     }),
-    // loggingWinston,
   ],
 });
