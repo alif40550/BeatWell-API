aac64fc280cf4406623598df58793a37a4aca741
ballll16 <ahmadhamidbalya@gmail.com>
2024-12-09 17:46:55 +0700

    🧪 test(histories): test get endpoint

--- test/history.test.ts
+++ test/history.test.ts
@@ -1,61 +1,55 @@
-// import { HistoryResponse } from '../src/models/History';
-// import { getHistories, signIn } from './services';
-// import startServer from '../src/libs/server';
+import { HistoryResponse } from '../src/models/History';
+import { deleteHistory, getHistories, getTokenTest } from './services';
+import { tokens, user } from './testEnv';
 
-// const server = startServer();
+/*
+ * TODO:
+ * 1. make get history by id test case,
+ */
 
-// beforeAll(() => {});
+describe('GET /histories', () => {
+  it('should return a response with 200', async () => {
+    const [userToken, userId] = (await getTokenTest()) as string[];
+    tokens.push(userToken);
+    user.id = userId;
+    const token = tokens[tokens.length - 1];
+    const res = await getHistories(token, userId);
+    expect(res.body.error).toBe(false);
 
-// afterAll(() => {
-//   server.close();
-//   // clear the history
-// });
+    expect(res.status).toBe(200);
+  });
+  it('should return a response with json', async () => {
+    const token = tokens[tokens.length - 1];
+    const userId = user.id;
+    const res = await getHistories(token, userId);
+    expect(res.type).toBe('application/json');
+  });
+  it('should return a array of history', async () => {
+    const token = tokens[tokens.length - 1];
+    const userId = user.id;
+    const { body } = await getHistories(token, userId);
+    const histories = body.data as Array<HistoryResponse>;
+    expect(typeof histories).toBe('object');
+  });
+  it('should return a error 401 if token is invalid', async () => {
+    const token = 'jasfbaskjdb';
+    const userId = '2';
+    const res = await getHistories(token, userId);
+    expect(res.body.error).toBe(true);
+    expect(res.status).toBe(401);
+  });
+});
 
-// /*
-//  * TODO:
-//  * 1. make get history by id test case,
-//  */
-
-// describe('GET /histories', () => {
-//   it('should return a response with 200', async () => {
-//     const token = await signIn();
-//     const res = await getHistories(token);
-//     expect(res.body.error).toBe(false);
-//     expect(res.status).toBe(200);
-//   });
+describe('DELETE /histories', () => {
+  it('should return a response with 200', async () => {
+    const token = tokens[tokens.length - 1];
+    const res = await deleteHistory(token, '1');
+    expect(res.body.error).toBe(false);
+    expect(res.status).toBe(200);
+  });
 //   it('should return a response with json', async () => {
-//     const token = await signIn();
-//     const res = await getHistories(token);
-//     expect(res.type).toBe('application/json');
-//   });
-//   it('should return a array of history', async () => {
-//     const token = await signIn();
-//     const { body } = await getHistories(token);
-//     const histories = body.data as Array<HistoryResponse>;
-//     console.log('🚀 ~ it ~ typeof histories:', typeof histories);
-//     expect(typeof histories).toBe('object');
-//   });
-//   it('should return a error 401 if token is invalid', async () => {
-//     const token = 'jasfbaskjdb';
-//     const res = await getHistories(token);
-//     expect(res.body.error).toBe(true);
-//     expect(res.status).toBe(401);
-//   });
-// });
-
-// describe('DELETE /histories', () => {
-//   beforeAll(() => {
-//     console.log('beforeAll');
-//   });
-//   afterAll(() => {});
-//   it('should return a response with 200', async () => {
-//     const token = await signIn();
-//     const res = await getHistories(token);
-//     expect(res.body.error).toBe(false);
-//     expect(res.status).toBe(200);
-//   });
-//   it('should return a response with json', async () => {
-//     const token = await signIn();
+//     const { body } = await signIn();
+//     const token = body.data.token;
 //     const res = await getHistories(token);
 //     expect(res.type).toBe('application/json');
 //   });
@@ -70,4 +64,4 @@
 //   //     expect(res.body.error).toBe(true);
 //   //     expect(res.status).toBe(401);
 //   //   });
-// });
+});
